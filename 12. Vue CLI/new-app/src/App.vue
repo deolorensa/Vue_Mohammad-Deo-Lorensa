2a4f81d1b0c7fc8e3d0ba0b3fd6ec016b285cf01
deolorensa <lorensadeo10@gmail.com>
2022-03-18 18:41:33 +0700

    section 12

---
+++ 12. Vue CLI/new-app/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <div id="app">
+        <ol>
+            <li v-for="toDo in toDos" :key="toDo">{{ toDo }}</li>
+        </ol>
+        <input type="text" v-model="newToDo">
+        <button @click="addToDo">Tambahkan</button>
+        <p v-if="showMessage">Hebat!</p>
+    </div>
+    
+  </div>
+</template>
+    <script>
+        const opts = {
+            el : '#app',
+            data: {
+                toDos:[],
+                newToDo:"",
+                showMessage: false
+            },
+            methods: {
+                addToDo(){
+                    this.toDos.push(this.newToDo)
+                    this.listChecker()
+                },
+                listChecker(){
+                    if(this.toDos.length > 3){
+                        this.showMessage=true
+                    } 
+                }
+            },
+            
+        }
+        var app = new Vue(opts)
+    </script>
