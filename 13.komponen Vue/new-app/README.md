ddf293f8c93f61ce471acf8c7c9c6b96714f6a29
deolorensa <lorensadeo10@gmail.com>
2022-03-23 17:55:40 +0700

    section 13

---
+++ 13.komponen Vue/new-app/README.md
@@ -0,0 +1,37 @@
+Resume Section 13 Komponen Vue
+
+Komponen merupakan salah satu hal penting yang harus diperhatikan pada saat pengembangan sebuah aplikasi di sisi frontend. Masalah Reusability menjadi hal yang lumayan sulit untuk dikelola seiring dengan perkembangan codebase dari aplikasi yang semakin kompleks.
+
+Manfaat Menggunakan Components
+Mudahnya dengan component memungkinkan kita untuk membuat suatu fitur lebih cepat karena kita tidak perlu membuatnya dari awal dan tinggal memanfaatkan component yang sudah ada, selain itu juga menghindari kita dari membuat kode yang sama di component yang lain.
+
+Masalah yang timbul ketika menggunakan Props
+1. Semakin banyak requirement maka semakin kompleks component tersebut
+2. Component menjadi tidak terlalu fleksibel
+3. Semakin banyak kondisi/percabangan pada component
+4. Satu component menjadi mempunyai banyak tanggung jawab
+
+# new-app
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ 13.komponen Vue/new-app/src/components/toDo.vue
@@ -0,0 +1,60 @@
+<template>
+  <div id="hey">
+      <ol>
+          <div v-if="!isEditing">
+            <li v-for="(item, index) of task" v-bind:key="index"> {{ item }}
+            <button @click="remove(index)">hapus</button>
+            <button @click="editTodo">edit</button>
+            </li>
+          </div>
+          
+          <div v-else>
+            <li v-for="(item, index) of task" v-bind:key="index"> {{ item }}
+            <button @click="remove(index)">hapus</button>
+            <button @click="updateTodo">update</button>
+            </li>
+          </div>
+          
+      </ol>
+    <p v-if="task.length < 1">Dokumen Kosong</p>
+    <input type="text" v-model="taskBaru" @keyup.enter="submit" />
+    <button @click="submit">Tambahkan</button>
+    <p v-if="task.length > 3">Hebat!</p>
+  </div>
+</template>
+<script>
+export default {
+  name: "HeyYou",
+  data: function () {
+    return {
+      task: [],
+      taskBaru: "",
+      isEditing: false,
+      selectedTodo: null
+    };
+  },
+  methods: {
+    submit() {
+      this.task.push(this.taskBaru);
+      this.taskBaru = "";
+    },
+    remove(id){
+        this.$delete(this.task, id);
+    },
+    updateTodo() {
+        this.task.splice(this.selectedIndex, 1, this.item)
+        this.item = ''
+        this.isEditing = false
+        },
+    editTodo(index, item) {
+        this.isEditing = true
+        this.item = item
+        this.selectedIndex = index
+        },
+  },
+};
+</script>
+
+<style>
+
+</style>
